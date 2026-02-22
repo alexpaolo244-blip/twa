90d5388961b57ef175482eca2f49c4dc7c27f76d
alexpaolo244-blip <alexpaolo244@gmail.com>
2026-02-22 15:17:21 +0000

    Create MainActivity.java

---
+++ app/src/main/java/com/shofyou/app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.shofyou.app;
+import com.google.androidbrowserhelper.trusted.LauncherActivity;
+
+public class MainActivity extends LauncherActivity {
+    // لا تكتب شيئاً هنا، LauncherActivity ستقوم بكل العمل
+}
